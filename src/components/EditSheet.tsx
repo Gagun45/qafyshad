8c56e4d5e3db9bff25a4099f1299e24de6a904e1
Gagun45 <selyanchyn45@gmail.com>
2025-05-15 14:42:06 +0300

    edit profile started

--- src/components/EditSheet.tsx
+++ src/components/EditSheet.tsx
@@ -1,5 +1,7 @@
 import { EditForm } from "./EditForm";
 import { Button } from "./ui/button";
+import { ScrollArea } from "./ui/scroll-area";
+import { Separator } from "./ui/separator";
 import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
 
 export default function EditSheet() {
@@ -8,9 +10,12 @@ export default function EditSheet() {
       <SheetTrigger asChild>
         <Button>Edit Profile</Button>
       </SheetTrigger>
-      <SheetContent side="top" className="min-h-1/2 items-center">
+      <SheetContent side="top" className="min-h-1/2">
         <SheetTitle>Edit Profile</SheetTitle>
-        <EditForm/>
+        <Separator />
+        <ScrollArea className="overflow-auto flex-1">
+          <EditForm />
+        </ScrollArea>
       </SheetContent>
     </Sheet>
   );
